68b3fe5d137b7e64bd8d4f10fc39a452f8883f07
Grace Akpan <opps4lrng@gmail.com>
2021-08-21 01:50:42 -0600

    updated text in todo and time calculator

--- packages/timeCalculator/src/components/index.js
+++ packages/timeCalculator/src/components/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default () => {
-  return <h3>Time Calculator!!ldkjsfoiauro92!</h3>;
+  return (
+    <>
+      <h3>Time Calculator!!ldkjsfoiauro92!</h3>
+      <div>I am the time calculator</div>
+    </>
+  );
 };
--- packages/toDoList/src/components/index.js
+++ packages/toDoList/src/components/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default () => {
-  return <h3>Todo List!!lajsdfoiwurouw</h3>;
+  return (
+    <>
+      <h3>Todo List!!lajsdfoiwurouw</h3>
+      <div>I am the Todo</div>
+    </>
+  );
 };
