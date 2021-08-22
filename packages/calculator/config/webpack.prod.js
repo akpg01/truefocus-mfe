6fee82847e4b85cd178d955ad58bb14fc231de85
Grace Akpan <opps4lrng@gmail.com>
2021-08-21 21:24:18 -0600

    updates to calculator

--- packages/calculator/config/webpack.prod.js
+++ packages/calculator/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    pulicPath: "/calculator/latest/",
+    publicPath: "/calculator/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
