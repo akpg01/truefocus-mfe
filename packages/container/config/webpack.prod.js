0206b1442a9cfc7ebc9a0061d2c01409b3a9d828
Grace Akpan <opps4lrng@gmail.com>
2021-08-22 22:15:20 -0600

    added import statement to each webpack-prod files

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const commonConfig = require("./webpack.common");
 const packageJson = require("../package.json");
 
@@ -29,6 +30,14 @@ const prodConfig = {
       shared: packageJson.dependencies,
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
 
 module.exports = merge(commonConfig, prodConfig);
--- packages/schedule/config/webpack.prod.js
+++ packages/schedule/config/webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
 
